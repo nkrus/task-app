c386b05b6f9dd998a1107d432f7df4ec4ead36d8
Nikita Solovyev <nik1rus2moscow3@gmail.com>
2019-10-09 17:38:00 +0300

    Updates

---
+++ precache-manifest.a509c718313dc374cd220bddcabe4e8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f49d8e7ff91a2705acf60a218c1612b",
+    "url": "/task-app/index.html"
+  },
+  {
+    "revision": "a66d2570f1ed8f228066",
+    "url": "/task-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "40362530c8f46811d731",
+    "url": "/task-app/static/js/2.cdfa7608.chunk.js"
+  },
+  {
+    "revision": "a66d2570f1ed8f228066",
+    "url": "/task-app/static/js/main.43a8193d.chunk.js"
+  },
+  {
+    "revision": "c64572118887ad000682",
+    "url": "/task-app/static/js/runtime-main.460baac1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/task-app/static/media/logo.25bf045c.svg"
+  }
+]);
